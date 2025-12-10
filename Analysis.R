66f3c77ad097da8e1a6f711bec05fc729dde7cb3
Abdulbasit-khan1 <ak24ake@herts.ac.uk>
2025-12-10 14:08:57 -0800

    Member 1: Added data loading, cleaning, and preprocessing code

---
+++ Analysis.R
@@ -0,0 +1,55 @@
+# Correlation between Obesity (%) and
+# Confirmed COVID-19 cases (% of population)
+#Member 1 Abdul Basit Khan Load packages, import data, clean dataset
+
+# 1. Loaded the dataset
+
+diet_data <- read_csv("Food_Supply_Quantity_kg_Data.csv")
+df <- diet_data
+
+# Checking the names
+names(df)
+
+# We Make sured obesity simple name
+names(df)[which(names(df) == "Obesity")]   <- "obesity"
+#now we make sure Confirmed cases have simple name
+names(df)[which(names(df) == "Confirmed")] <- "confirmed"
+
+# converting to numeric to read as character 
+df$obesity   <- as.numeric(df$obesity)
+df$confirmed <- as.numeric(df$confirmed)
+
+## 2. Now Cleanning the data
+
+# Removing the unrealistic values and missing values 
+df2 <- subset(df,
+              !is.na(obesity)   &
+              !is.na(confirmed) &
+              obesity   > 0  & obesity   < 60 &
+              confirmed >= 0 & confirmed < 30)
+# below code shows Number of countries
+cat("Number of countries used:", nrow(df2), "\n")
+
+## --- Member 2: Histograms to check distributions ---
+# (Member 2 will add their code here later)
+
+
+
+## --- Member 3: Now we will do Correlation test  ---
+# (Member 3 will add their code here later)
+
+
+
+## --- Member 4: Scatterplot with regression line  ---
+# (Member 4 will add their code here later)
+
+
+
+## --- Member 5: Saved plots as PNG ---
+# (Member 5 will add their code here later)
+
+
+
+
+
+
